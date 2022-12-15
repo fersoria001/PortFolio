251e47c72b7928798e0fa46d6edd0f3ad8920cb4
¨FerSoria¨ <bercho001@gmail.com>
2022-12-15 12:43:18 -0300

    resize

--- portfolio/src/PaginaPrincipal/SobreMi.jsx
+++ portfolio/src/PaginaPrincipal/SobreMi.jsx
@@ -4,7 +4,7 @@ export const SobreMi = () => {
   return (
     <div className='container-fluid'>
         <div className='row justify-content-center p-5'>
-            <div className='col-sm-4'>
+            <div className='col-md-6'>
     <p className="text-justify text-md-center">
         Hi! I'm Fer! I study Computer Engineering at the UNRN and I love programming, also
         I course the OracleG3 full stack web development program, the main subject is Front-End
