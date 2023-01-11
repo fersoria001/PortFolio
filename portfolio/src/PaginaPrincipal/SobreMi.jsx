f9c678b835afc664dfb4f2b758726a1fb1937bfc
¨Fercho¨ <bercho001@gmail.com>
2023-01-10 22:07:30 -0300

    fixed message

--- portfolio/src/PaginaPrincipal/SobreMi.jsx
+++ portfolio/src/PaginaPrincipal/SobreMi.jsx
@@ -7,8 +7,7 @@ export const SobreMi = () => {
             <div className='col-md-6'>
     <p className="text-justify text-md-center">
         Hi! I'm Fer! I study Computer Engineering at the UNRN and I love programming, also
-        I course the OracleG3 full stack web development program, the main subject is Front-End
-        programming but there is also space to learn backend in Java with MySQL databases.
+        I'm a certified FullStack Java Web Developer + MySQL.
         My Favourites jobs are going to the Gym and trekking in the beautiful mountains of
         San Carlos de Bariloche. 
     </p>
